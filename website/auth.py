05996b6d7858ca66bb533d9137d5e4d43dfe85e5
Francis J.B <8799137+francisbjr@users.noreply.github.com>
2024-01-11 22:48:49 -0500

    Change file extensions
    * Rename .html files to .jinja files
    * Jinja template syntax will now highlight (in VS Code)

--- website/auth.py
+++ website/auth.py
@@ -4,12 +4,12 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login') # Login page URL
 def login():
-    return render_template("login.html")
+    return render_template("login.jinja")
 
 @auth.route('/logout') # Logout page URL
 def logout():
-    return render_template("logout.html")
+    return "logout" #TODO: Render logout
 
 @auth.route('/sign-up') # Sign Up page URL
 def sign_up():
-    return render_template("sign_up.html")
+    return render_template("sign_up.jinja")
